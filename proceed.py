9d9d4fa38f557821734bb473c7123163af538e0e
statik2002 <statik2002@mail.ru>
2022-12-19 15:53:58 +0800

    Разбил все по функциям

--- proceed.py
+++ proceed.py
@@ -1,38 +1,18 @@
 from pprint import pprint
 from environs import Env
-import requests
+from utils import get_shops, get_today_proceed
 
 env = Env()
 env.read_env()
 
 api_key = env('API_KEY')
 
-url = 'https://api.kontur.ru/market/v1/shops'
+shops = get_shops(api_key)
 
-header = {
-    'x-kontur-apikey': api_key
-}
+today_proceed = get_today_proceed(api_key, shops[0]['id'])
 
-response = requests.get(url, headers=header)
-response.raise_for_status()
-
-shops = response.json()
-
-first_shop = shops['items'][0]
-
-shop_id = first_shop['id']
-shop_name = first_shop["name"]
-shop_organizationId = first_shop["organizationId"]
-shop_address = first_shop["address"]
+pprint(today_proceed)
 
 
-cheques_url = 'https://api.kontur.ru/market/v1/shops/{}/cheques'.format(shop_id)
 
-response = requests.get(cheques_url, headers=header)
-response.raise_for_status()
-
-proceed = response.json()['items']
-
-for check in proceed:
-    print(check['totalPrice'])
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def get_product(shop_id, product_id, api_key):
     url = 'https://api.kontur.ru/market/v1/shops/{}/products/{}'.format(shop_id, product_id)
 
@@ -11,3 +12,30 @@ def get_product(shop_id, product_id, api_key):
     response.raise_for_status()
 
     return response.json()
+
+
+def get_shops(api_key):
+    url = 'https://api.kontur.ru/market/v1/shops'
+
+    header = {
+        'x-kontur-apikey': api_key
+    }
+
+    response = requests.get(url, headers=header)
+    response.raise_for_status()
+
+    return response.json()['items']
+
+
+def get_today_proceed(api_key, shop_id):
+    header = {
+        'x-kontur-apikey': api_key
+    }
+
+    proceed_url = 'https://api.kontur.ru/market/v1/shops/{}/cheques'.format(shop_id)
+
+    response = requests.get(proceed_url, headers=header)
+    response.raise_for_status()
+
+    return response.json()['items']
+
